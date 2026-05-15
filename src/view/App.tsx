7d855e994a55532d402276422978b5e7805c61f2
Hughe5 <1767604480@qq.com>
2026-05-15 15:00:59 +0800

    chore: update version & format

--- src/view/App.tsx
+++ src/view/App.tsx
@@ -222,7 +222,12 @@ const App = forwardRef<AppRef, AppProps>(({onReady}, ref) => {
               aria-label="新对话"
               onClick={handleCreate}
             >
-              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 1024 1024">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="18"
+                viewBox="0 0 1024 1024"
+              >
                 <path d="M597.333 128v85.333H170.667v571.094l75.221-59.094h607.445V426.667h85.334V768A42.667 42.667 0 0 1 896 810.667H275.413L85.333 960V170.667A42.667 42.667 0 0 1 128 128h469.333zm213.334 0V0H896v128h128v85.333H896v128h-85.333v-128h-128V128h128z" />
               </svg>
             </button>
@@ -264,7 +269,12 @@ const App = forwardRef<AppRef, AppProps>(({onReady}, ref) => {
                 style={{display: isChatting ? 'none' : 'flex'}}
               >
                 <div className="submit-icon">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    fill="currentColor"
+                  >
                     <path d="M9 16V6.414L5.707 9.707a1 1 0 1 1-1.414-1.414l5-5 .076-.068a1 1 0 0 1 1.338.068l5 5 .068.076a1 1 0 0 1-1.406 1.406l-.076-.068L11 6.414V16a1 1 0 1 1-2 0Z" />
                   </svg>
                 </div>
@@ -279,7 +289,12 @@ const App = forwardRef<AppRef, AppProps>(({onReady}, ref) => {
                 style={{display: isChatting ? 'flex' : 'none'}}
               >
                 <div className="stop-icon">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    fill="currentColor"
+                  >
                     <path d="M4.5 5.75c0-.69.56-1.25 1.25-1.25h8.5c.69 0 1.25.56 1.25 1.25v8.5c0 .69-.56 1.25-1.25 1.25h-8.5c-.69 0-1.25-.56-1.25-1.25v-8.5Z" />
                   </svg>
                 </div>
--- src/view/components/MessageItem.tsx
+++ src/view/components/MessageItem.tsx
@@ -136,7 +136,12 @@ const MessageItem = forwardRef<HTMLDivElement, MessageItemProps>(({message}, ref
             {isCopied ? (
               <span>Copied</span>
             ) : (
-              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 1024 1024">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="18"
+                viewBox="0 0 1024 1024"
+              >
                 <path d="M298.667 256V128a42.667 42.667 0 0 1 42.666-42.667h512A42.667 42.667 0 0 1 896 128v597.333A42.667 42.667 0 0 1 853.333 768h-128v128c0 23.552-19.2 42.667-42.965 42.667H170.965A42.71 42.71 0 0 1 128 896l.128-597.333c0-23.552 19.2-42.667 42.923-42.667h127.616zm-85.248 85.333-.086 512H640v-512H213.419zM384 256h341.333v426.667h85.334v-512H384V256z" />
               </svg>
             )}
