3a69cb7d2a06bdc4b9f2a8fdd4e91436d82f9ea1
Hughe5 <1767604480@qq.com>
2026-05-24 16:46:38 +0800

    chore: vite 去掉代码分割

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,11 @@
 import {defineConfig} from 'vite';
-import {manualChunks} from './vite.config.shared';
 
 export default defineConfig({
   build: {
-    outDir: 'dist/es',
     lib: {
       entry: './src/index.ts',
       name: 'AIClientSDK',
       fileName: 'ai-client-sdk',
-      formats: ['es'],
-    },
-    rollupOptions: {
-      output: {
-        manualChunks,
-      },
     },
   },
   server: {
